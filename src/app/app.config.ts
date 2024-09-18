be9fa1d2fc8febff8a7e4cbf10d76363302dc2b5
Lluc Llull <lluc@clicktotravel.es>
2024-09-18 08:47:58 +0200

    add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+
 import {provideHttpClient, withFetch} from "@angular/common/http";
+import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
+import {environment} from "../environments/environment";
+import {getAuth, provideAuth} from "@angular/fire/auth";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch())]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth())
+  ]
 };
--- src/app/news/auth/register/register.component.ts
+++ src/app/news/auth/register/register.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {Auth, createUserWithEmailAndPassword} from "@angular/fire/auth";
+import {Router} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
+  selector: 'register',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  errorMessage: string = '';
+
+  constructor(private auth: Auth, private router: Router) {}
+
+  onRegister() {
+    if (this.password !== this.confirmPassword) {
+      this.errorMessage = 'Las contraseñas no coinciden';
+      return;
+    }
+
+    if (this.email && this.password) {
+      createUserWithEmailAndPassword(this.auth, this.email, this.password)
+        .then(() => {
+          this.router.navigate(['/news']);
+        })
+        .catch(error => {
+          this.errorMessage = 'Error en el registro: ' + error.message;
+        });
+    } else {
+      this.errorMessage = 'Por favor, complete todos los campos.';
+    }
+  }
 
 }
